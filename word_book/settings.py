99638fc494eaf1c23c447db087e5fda6ad172c27
foonia <ljh9032a@naver.com>
2018-02-12 16:28:13 +0900

    install django-extensions

--- word_book/settings.py
+++ word_book/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '%-n(-wvrd4)syvcrx3_!*qfukh80vpl=&*l^l04!707r(i5fu)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     
     'bootstrap3',
+    'django_extensions',
 
     'word',
 ]
