cdae6f08b09588dc9e824fca1fbcbbfa9ec77d2d
hoon <ljh9032a@naver.com>
2018-02-20 22:22:12 +0900

    modified word count

--- word/models.py
+++ word/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class Word(models.Model):
-    word = models.CharField(max_length=20)
-    meaning = models.CharField(max_length=20)
+    word = models.CharField(max_length=50)
+    meaning = models.CharField(max_length=50)
     count = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
 
