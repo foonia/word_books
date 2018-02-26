ce1b258be3d3c20faeb0b3cac401a9930717cf16
leejunghoon <ljh9032a@naver.com>
2018-02-26 10:54:31 +0900

    add missing file for accounts

---
+++ accounts/views.py
@@ -0,0 +1,20 @@
+from django.conf import settings
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            return redirect(settings.LOGIN_URL) # settings.LOGIN_URL='accounts/login/'
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'accounts/signup_form.html', {
+        'form':form,    
+    })
+
+@login_required
+def profile(request):
+    return render(request, 'accounts/profile.html')
